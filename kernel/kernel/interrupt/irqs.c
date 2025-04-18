b256c79b189b015ec3de7e3060cfd6dcff7829fd
De-Rosa <lucadr222@gmail.com>
2025-04-19 00:00:14 +0100

    Add scan code set, finish keyboard typing test

--- kernel/kernel/interrupt/irqs.c
+++ kernel/kernel/interrupt/irqs.c
@@ -1,15 +1,9 @@
 #include <kernel/interrupt/keyboard/ps2.h>
-#include <kernel/interrupt/idt.h>
+#include <kernel/interrupt/keyboard/keyboard.h>
 #include <kernel/interrupt/irqs.h>
-#include <stdio.h>
 #include <stdint.h>
 
 extern void outb(uint16_t port, uint8_t operand);
-
-void keyboard_irq(struct registers* regs) {
-	uint8_t scancode = read_response();
-	printf("a");
-}
 
 void install_irqs(void) {
 	install_irq_handler(1, keyboard_irq);
